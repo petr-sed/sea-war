a79094c5731e4f6b116bed5332af6d1233068713
Petr <petr-sed@rambler.ru>
2019-04-29 06:45:21 +0300

    игра морской бой на python

---
+++ ship.py
@@ -0,0 +1,14 @@
+class Ship:
+    def __init__(self, palubs, [coords], ungle):
+        self.palubs = palubs
+        self.coords = coords
+        self.ungle = ungle
+
+    def position(self):
+        screen = turtle.Screen()
+        answer = screen.textinput("Nose point of your 4-palubs ship:")
+
+
+
+
+
